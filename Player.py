8dbb7b34f5b5d84b3d813226ead6b870e23b2f6f
Kenny <ksiebert@mit.edu>
2014-04-14 01:26:35 -0400

    Added comments

--- Player.py
+++ Player.py
@@ -1,9 +1,24 @@
 from ShotZone import ShotZone
 
+# 2d array responsible for mapping x,y to corresponding spot zone
+"""
+Zone Diagram
+         <-15->
+ ----------------------         
+ 10 | 11 | 12 | 13 | 14
+ ----------------------
+ 5  | 6  | 7  | 8  | 9
+ ----------------------
+ 0  | 1  | 2  | 3  | 4
+
+ Zones 0-14 are 10'x10'
+ Zone 15 covers shots from beyond 30'
+ The basket would be located in zone 2
+""" 
+
 Zones = [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14], [15,15,15,15,15]]
 
 class Player:
-
 	def __init__(self, name):
 		self.name = name
 		self.shot_zones = []
@@ -12,16 +27,20 @@ class Player:
 			zone = ShotZone(i)
 			self.shot_zones.append(zone)
 
-
+	# Adds a shot from x,y of shot type to the current player
+	# shot is mapped to zone and stored in appropriate shot zone
 	def add_shot(self, x, y, shot_type, make):
 		zone = self.get_zone(x,y)
 		self.shot_zones[zone].shot(shot_type, make)
 
-
+	# Searchs for a given shot type in the zone that x,y maps too
+	# returns an array of format [attempts, makes]
 	def get_attempts_makes(self, x, y, shot_type):
 		zone = self.get_zone(x,y)
 		return self.shot_zones[zone].get_attempts_makes(shot_type)
 
+	# takes in an x, y coordinate
+	# maps the x, y coordinate to one of 16 zones
 	def get_zone(self, x, y):
 		y_index = 0
 		if y > 30:
@@ -32,6 +51,7 @@ class Player:
 		
 		return Zones[y_index][x_index]
 
+	# Format player to string method
 	def __str__(self):
 		outstring = "Player: " + self.name + "\n"
 		for zone in self.shot_zones:
--- ShotZone.py
+++ ShotZone.py
@@ -1,9 +1,13 @@
+# Class to represent a shotzone
+# stores a map of all different shot types occuring in zone
 class ShotZone:
 
 	def __init__(self, zone):
 		self.shots = {} # {type: [attempts, makes]}
 		self.zone = zone
 
+	# Adds a shot of shot type
+	# make indicates whether or not shot was made
 	def shot(self, shot_type, make):
 		# add potentially new shot type
 		if shot_type not in self.shots:
@@ -16,6 +20,8 @@ class ShotZone:
 			self.shots[shot_type][0] += 1
 		return
 
+	# gets all shots of shot type in the zone
+	# returns array of format [attempts, makes]
 	def get_attempts_makes(self, shot_type):
 		if not shot_type:
 			output = [0,0]
@@ -25,6 +31,7 @@ class ShotZone:
 			return output
 		return self.shots[shot_type]
 
+	# format shot zone class to string
 	def __str__(self):
 		outstring = ''
 		outstring += 'Zone: ' + str(self.zone) + '\n'
